dee632c8c91e119f07dbc7d6726dc9c2fe586411
yusuke <yusuke@localhost>
2023-05-06 23:02:32 +0900

    課題1.3 新規作成

---
+++ HelloWorld_TakaiYusuke/src/sample/SelfIntroduction.java
@@ -0,0 +1,22 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		// 氏名
+		String name = "高井 雄輔";
+		// 年齢
+		int age = 30;
+		// 趣味
+		String hobby = "バスケ";
+		
+		/*
+		 *  自己紹介文を出力する処理
+		 */
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+
+	}
+
+}
